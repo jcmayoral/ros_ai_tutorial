ebd9292b0d3465c6f5bddedc3818d2233e7f719c
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-02-12 11:33:59 +0100

    simple cpp library and cpp executable

---
+++ cpp_basics/cpp_library/my_library/src/my_class.cpp
@@ -0,0 +1,9 @@
+#include <my_library/my_class.h>
+
+MyClass::MyClass(){
+    std::cout << "This is the constructor" << std::endl;
+}
+
+MyClass::~MyClass(){
+    std::cout << "This is the destructor" << std::endl;
+}
