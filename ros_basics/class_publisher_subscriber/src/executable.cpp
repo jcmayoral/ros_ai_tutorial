1b2cc2ee1c564d07a4e8d1b8882edb275c99ca28
Jose Mayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-02-18 17:21:54 +0100

    class_publisher_subscriber

---
+++ ros_basics/class_publisher_subscriber/src/executable.cpp
@@ -0,0 +1,6 @@
+#include <class_publisher_subscriber/main_class.h>
+
+int main(int argc, char** argv){
+	ros::init(argc,argv,"publisher_subscriber_node");
+	MainClass main_class;
+};
---
+++ ros_basics/class_publisher_subscriber/src/main_class.cpp
@@ -0,0 +1,17 @@
+#include <class_publisher_subscriber/main_class.h>
+
+MainClass::MainClass(): nh_("~"){
+	main_subscriber_ = nh_.subscribe("main_topic", 1, &MainClass::main_cb,this);
+	output_publisher_ = nh_.advertise<std_msgs::String>("node_output", 1);
+	ros::spin();
+};
+
+MainClass::~MainClass(){
+}
+
+void MainClass::main_cb(const std_msgs::String::ConstPtr msg){
+	ROS_INFO_STREAM("Your message is " << msg-> data);
+	std_msgs::String output_msg;
+	output_msg.data = "hello ROS world";
+	output_publisher_.publish(output_msg);
+};
