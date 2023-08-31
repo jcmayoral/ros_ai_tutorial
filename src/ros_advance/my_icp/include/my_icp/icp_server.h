11d8b2d879f4849dc97bec467afea51f45b9af68
Jose Mayoral <jocamaba1989@gmail.com>
2023-08-31 15:05:59 -0400

    0.0.3

--- src/ros_advance/my_icp/include/my_icp/icp_server.h
+++ src/ros_advance/my_icp/include/my_icp/icp_server.h
@@ -1,6 +1,16 @@
+#include<ros/ros.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/registration/icp.h>
 
+#include <sensor_msgs/PointCloud2.h>
 
 class ICP_SERVER{
-	ICP_SERVER();
-	~ICP_SERVER();
+	public:
+		ICP_SERVER();
+		~ICP_SERVER();
+		void cloud_cb(const sensor_msgs::PointCloud2& cloud);
+	private:
+		ros::NodeHandle nh_;
 };
--- src/ros_advance/my_icp/src/icp_server.cpp
+++ src/ros_advance/my_icp/src/icp_server.cpp
@@ -8,3 +8,16 @@ ICP_SERVER::ICP_SERVER(){
 ICP_SERVER::~ICP_SERVER(){
 	
 }
+
+void ICP_SERVER::cloud_cb(const sensor_msgs::PointCloud2& cloud_msg){
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud;
+	pcl::fromROSMsg(cloud_msg, *input_cloud);
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr output_cloud;
+	pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+
+	icp.setMaximumIterations(100);
+	icp.setInputSource(input_cloud);
+
+}
